3d4469370922f435b7956cf602295f891fcee581
Woshiajuana <979703986@qq.com>
2021-06-29 14:01:04 +0800

    subcode

--- unionpay-coupon/src/router/index.js
+++ unionpay-coupon/src/router/index.js
@@ -3,30 +3,36 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 
-const routes = (s => {
-    const r = [
-        {
-            path: '/',
-            name: 'Home',
-            component: () => import(/* webpackChunkName: "Home" */ 'src/views/Home'),
-            meta: { depth: 1, title: '首页' },
-        },
-        {
-            path: '/about',
-            name: 'About',
-            // component: () => import(/* webpackChunkName: "about" */ 'src/views/About'),
-            component: require('src/views/About').default,
-            meta: { depth: 2, title: '关于' },
-        }
-    ];
-    s.keys().map(k => s(k).default).flat();
-    return r;
-})(require.context('./modules/', true, /\.js$/));
+const routes = (s => [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "Home" */ 'src/views/Home'),
+        meta: { depth: 1, title: '首页' },
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import(/* webpackChunkName: "about" */ 'src/views/About'),
+        // component: require('src/views/About').default,
+        meta: { depth: 2, title: '关于' },
+    },
+    ...s.keys().map(k => s(k).default).flat(),
+    {
+        path: '*',
+        redirect: '/',
+    },
+])(require.context('./modules/', true, /\.js$/));
 
 const router = new VueRouter({
     mode: 'hash',
     base: process.env.BASE_URL,
-    routes
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title || '';
+    next();
 });
 
 export default router
