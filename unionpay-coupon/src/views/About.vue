1482694e18d7597748edb356303180a8e211591e
Woshiajuana <979703986@qq.com>
2021-06-29 14:34:40 +0800

    subcode

--- unionpay-coupon/src/views/About.vue
+++ unionpay-coupon/src/views/About.vue
@@ -1,16 +1,28 @@
+
 <template>
     <div class="about">
         <h1>About</h1>
+        <van-button @click="$router.go(-1)">《返回</van-button>
     </div>
 </template>
 
+<script>
+    import { Button as VanButton } from 'vant'
+    export default {
+        components: {
+            VanButton,
+        }
+    }
+</script>
+
 <style lang="scss">
-@import "src/assets/scss/define";
-.about{
-    @extend %df;
-    @extend %aic;
-    @extend %jcc;
-    font-size: j(78);
-    background-color: #ffb642;
-}
+    @import "src/assets/scss/define";
+    .about{
+        @extend %df;
+        @extend %aic;
+        @extend %fdc;
+        @extend %jcc;
+        font-size: j(78);
+        background-color: #ffb642;
+    }
 </style>
--- unionpay-coupon/src/views/Home.vue
+++ unionpay-coupon/src/views/Home.vue
@@ -1,16 +1,39 @@
+
 <template>
     <div class="home">
         <h1>{{ new Date() | filterDate }}</h1>
+        <van-button @click="$router.push('/about')">跳转到关于页面</van-button>
+        <van-button @click="handlePromise">执行异步函数</van-button>
     </div>
 </template>
 
+<script>
+    import { Button as VanButton } from 'vant'
+
+    const fn = flag => new Promise((resolve, reject) => {
+        flag ? resolve(flag) : reject(flag);
+    })
+
+    export default {
+        methods: {
+            handlePromise () {
+                fn(0).then(res => console.log(`成功=>`, res)).toast();
+            }
+        },
+        components: {
+            VanButton,
+        }
+    }
+</script>
+
 <style lang="scss">
-@import "src/assets/scss/define";
-.home {
-    @extend %df;
-    @extend %aic;
-    @extend %jcc;
-    font-size: j(78);
-    background-color: #42bdff;
-}
+    @import "src/assets/scss/define";
+    .home {
+        @extend %df;
+        @extend %aic;
+        @extend %jcc;
+        @extend %fdc;
+        font-size: j(78);
+        background-color: #42bdff;
+    }
 </style>
