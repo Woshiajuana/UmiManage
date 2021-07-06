7b5d49225d5ec307cb8ec0214d474629ee98f0a8
Woshiajuana <979703986@qq.com>
2021-07-06 10:37:10 +0800

    subcode

---
+++ unionpay-coupon/src/views/mine/components/UserSection.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div class="user-section">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "src/assets/scss/define";
+
+</style>
