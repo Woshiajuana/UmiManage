d1099ff288de098076de6b9e96c4274619879f45
chenzhigang <979703986@qq.com>
2021-07-13 18:03:07 +0800

    subcode

---
+++ unionpay-coupon/build/cmd.js
@@ -0,0 +1,11 @@
+
+const path = require('path');
+const cmd = require('wow-cmd');
+
+cmd({
+    cmdPath: path.join(__dirname),
+    options: {
+        include: ['.cmd.js'],
+        exclude: [],
+    }
+});
